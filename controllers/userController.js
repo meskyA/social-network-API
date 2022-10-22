963c4be4ae701e9f28ac0b84ed542f18c9c164b2
meskyA <mesky2015@gmail.com>
2022-10-21 22:11:08 -0700

    works on server file

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,1 +1,126 @@
 const { User, Thought } = require("../models")
+
+const userController = {
+    getUsers(req, res) {
+        User.find()
+        .select("-__v")
+        .then((dbUserData ) => {
+            res.json(dbUserData);
+        })
+        .catch((err) => {
+            console.log(err);
+            res.status(500).json(err); 
+        });    
+    },
+// Get single user
+    getSingleUser(req, res) {
+        User.findOne({ _id: req.params.userId })
+        .select("-__v")
+        .populate("friends")
+        .populate("thoughts")
+        .then((dbUserData) => {
+            if (!dbUserData) {
+                return res.status(404).json({ message: "Error has occured" })
+            }
+            res.json(dbUserData);
+        })
+        .catch((err)  => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+    },
+
+// create new user
+createUser(req, res) {
+    User.create(req.body)
+    .then((dbUserData) => {
+        res.json(dbUserData);
+    })
+    .catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+},
+
+// update user
+
+updateUser(req, res) {
+    User.findOneAndUpdate( { _id: req.params.userId }, {
+        $set:req.body,
+    },
+    {
+        runValidators: true,
+        new: true,
+    }
+ )
+    .then((dbUserData) => {
+        if (!dbUserData) {
+            return res.status(404).json({ message: "Error has occured" });
+        }
+        res.json(dbUserData);
+    })
+    .catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+},
+// Delete a user and associated thoughts
+deleteUser(req, res) {
+    User.findOneAndDelete({ _id: req.params.userId })
+    .then((dbUserData) => {
+        if (!dbUserData) {
+            return res.status(404).json({ message: "Error has occured" });
+        }
+        // delete user's thougts
+        return Thought.deleteMany({ _id: { $in: dbUserData.thoughts }});
+    })
+    .then(() => {
+        res.json({ message: "Thought deleted."});
+    })
+    .catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+},
+
+// Add fiend
+
+addFriend(req, res) {
+    User.findOneAndUpdate( 
+        { _id: req.params.userId },
+        { $addToSet: { friends: req.params.fiendId }},
+        { new: true }
+    )
+    .then((dbUserData) => {
+        if (!dbUserData) {
+            return res.status(404).json({ message: "Error has occured."});
+        }
+        res.json(dbUserData);
+    })
+    .catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+},
+// Remove a friend
+
+removeFriend(req, res) {
+    User.findOneAndUpdate(
+        { _id: req.params.userId },
+        { $pull: { friends: req.params.friendId  }},
+        { new: true }
+    )
+    .then((dbUserData) => {
+        if (!dbUserData) {
+            return res.status(404).json({ message: "Error has occured."});
+        }
+        res.json(dbUserData);
+    })
+    .catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+},
+};
+
+module.exports = userController;
