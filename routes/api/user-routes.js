788a2c6776160c879259907914df1bdf74da2ba1
meskyA <mesky2015@gmail.com>
2022-10-21 21:10:22 -0700

    modifies index routes

---
+++ routes/api/user-routes.js
@@ -0,0 +1,25 @@
+const router = require("express").Router();
+
+const {
+    getUsers,
+    getSingleUser,
+    createUser,
+    updateUser,
+    deleteUser,
+    addFriend,
+    removeFriend,
+} = require("../../controllers/userController");
+
+// User API
+
+router.route('/').get(getUsers).post(createUser);
+
+// Single user API
+
+router.route('/:userId').get(getSingleUser).put(updateUser).delete(deleteUser);
+
+// Friend API
+
+router.route('/:userId/friends/:friendId').post(addFriend).delete(deleteFriend);
+
+module.exports = router;
